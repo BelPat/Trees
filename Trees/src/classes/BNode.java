9646716497eda2cc21a011caf413ebb6970090ff
nusatres <nusatres@mini-hp>
2014-07-21 19:23:37 +0200

    modificar comentarios

--- Trees/src/classes/BNode.java
+++ Trees/src/classes/BNode.java
@@ -15,9 +15,9 @@ public class BNode < T extends Comparable < T > > {
        
     /**
      * Constructor con parametros de la clase
-     * @param x es de tipo T el cual posee la keyrmación del nodo del arbol
-     * @param i es de tipo Nodo < T >  el cual posee el nodo del lado leftuierdo
-     * @param d es de tipo Nodo < T >  el cual posee el nodo del lado rightecho
+     * @param x es de tipo T el cual posee la información del nodo del arbol
+     * @param i es de tipo Nodo < T >  el cual posee el nodo del lado izquierdo
+     * @param d es de tipo Nodo < T >  el cual posee el nodo del lado derecho
      */
     public BNode ( T x, BNode < T >  i, BNode < T >  d ) {        
         this.key = x;
@@ -26,7 +26,7 @@ public class BNode < T extends Comparable < T > > {
     }
         
     /**
-     * Contructor vacio de la clase
+     * Contructor de la clase
      */
     public BNode( BNode < T >  x) {
         this.key = x.getKey();
@@ -44,16 +44,16 @@ public class BNode < T extends Comparable < T > > {
     }
     
     /**
-     * Método que retona la keyrmación del nodo
-     * @return un tipo T que contiene la keyrmación del nodo
+     * Método que retona la información del nodo
+     * @return un tipo T que contiene la información del nodo
      */
     public T getKey() {        
         return this.key;        
     }
     
     /**
-     * Método el cual retona el nodo leftuierdo
-     * @return un tipo Nodo < T >  el cual contiene el nodo leftuierdo
+     * Método el cual retona el nodo izquierdo
+     * @return un tipo Nodo < T >  el cual contiene el nodo izquierdo
      */
     
     public BNode < T >  getLeft() {        
@@ -61,24 +61,24 @@ public class BNode < T extends Comparable < T > > {
     }
     
     /**
-     * Método el cual retona el nodo rightecho
-     * @return un tipo Nodo  < T >  el cual contiene el nodo rightecho
+     * Método el cual retona el nodo derecho
+     * @return un tipo Nodo  < T >  el cual contiene el nodo derecho
      */
     public BNode < T >  getRight() {        
         return this.right;        
     }
     
     /**
-     * Método que modifica el nodo leftuierdo
-     * @param i es de tipo BNode < T >  que contiene el nodo leftuierdo
+     * Método que modifica el nodo izquierdo
+     * @param i es de tipo BNode < T >  que contiene el nodo izquierdo
      */
     public void setLeft ( BNode < T >  i ) {        
         this.left = i;        
     }
     
     /**
-     * Método que modifica el nodo rightecho
-     * @param d es de tipo NodoN < T >  que contiene el nodo rightecho
+     * Método que modifica el nodo derecho
+     * @param d es de tipo NodoN < T >  que contiene el nodo derecho
      */
     
     public void setRight( BNode < T >  d ) {        
@@ -87,7 +87,7 @@ public class BNode < T extends Comparable < T > > {
     
     /**
      * Método que modifica el contenido del nodo
-     * @param d de tipo T y contiene la keyrmación del nodo
+     * @param d de tipo T y contiene la información del nodo
      */
     public void setKey( T d ) {        
         this.key = d;        
