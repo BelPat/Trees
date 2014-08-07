8cf9c62a676acddab89813820b03d1864988b263
nusatres <nusatres@mini-hp>
2014-08-07 19:53:37 +0200

    Cambios en getLeft y getRight

--- src/datastructures/AVLNode.java
+++ src/datastructures/AVLNode.java
@@ -52,10 +52,10 @@ package datastructures;
     public AVLNode < T >  getLeft() {
      AVLNode< T > aux =null;
         if (super.getLeft() != null){
-            aux = new AVLNode<> (super.getLeft());
-            aux.balance = this.getBalance();
+           return (AVLNode<T>) super.getLeft();
+          /*  aux.balance = this.getBalance();
             aux.height = this.getHeight();
-            aux.root = this.getRoot();
+            aux.root = this.getRoot();*/
         }
         return aux;
     }
@@ -63,10 +63,11 @@ package datastructures;
       public AVLNode < T >  getRight() {
         AVLNode< T > aux = null;
             if (super.getRight() != null){
-            aux = new AVLNode<> (super.getRight());
+                return (AVLNode<T>) super.getRight();
+         /*   aux = new AVLNode<> (super.getRight());
             aux.balance = this.getBalance();
             aux.height = this.getHeight();
-            aux.root = this.getRoot();
+            aux.root = this.getRoot();*/
          }
                       
         return aux;
@@ -75,19 +76,7 @@ package datastructures;
         
         return root;
     }
-/*
-    public AVLNode < T >  getRight() {
-        return right;
-    }
 
-    public AVLNode < T >  getRoot() {
-        return root;
-    }
-
-    public T getKey() {
-        return key;
-    }
-*/
     public int getBalance() {
         return balance;
     }
@@ -96,22 +85,10 @@ package datastructures;
         return height;
     }
 
- /*   public void setLeft( AVLNode < T >  left ) {
-        this.left = left;
-    }
-
-    public void setRight( AVLNode < T >  right ) {
-        this.right = right;
-    }
-*/
     public void setRoot( AVLNode < T >  root ) {
         this.root = root;
     }
-/*
-    public void setKey( T key ) {
-        this.key = key;
-    }
-*/
+
     public void setBalance( int balance ) {
         this.balance = balance;
     }
--- src/datastructures/RBNode.java
+++ src/datastructures/RBNode.java
@@ -4,6 +4,8 @@
  */
 package datastructures;
 
+import static datastructures.RBNode.BLACK;
+
 /**
  *
  * @author nusatres
@@ -19,11 +21,41 @@ package datastructures;
   //  private T key;
 
     private RBNode < T >  root;
-    //private RBNode < T >  left, right;
+  //  private RBNode < T >  left, right;
     // the number of elements to the left of each node
     private int numLeft = 0;
     // the number of elements to the right of each node
     private int numRight = 0;
+        // the color of a node
+    private int color;
+    
+        RBNode(){
+        color = BLACK;
+        numLeft = 0;
+        numRight = 0;
+        root = null;
+        super.setRight( null);
+        super.setLeft( null);   
+    }
+
+    // Constructor which sets key to the argument.
+    RBNode( T key ){
+        super(key); 
+        color = BLACK;
+        numLeft = 0;
+        numRight = 0;
+        root = null;       
+       
+    }
+    
+    RBNode( Node<T> aux) {        
+        super.setKey(aux.getKey());
+        super.setRight( aux.getRight());
+        super.setLeft( aux.getLeft());   
+        this.root = null;       
+     
+    }
+
 
     public void setNumLeft(int numLeft) {
         this.numLeft = numLeft;
@@ -40,47 +72,17 @@ package datastructures;
     public int getNumRight() {
         return numRight;
     }
-    // the color of a node
-    private int color;
-
-    RBNode(){
-        color = BLACK;
-        numLeft = 0;
-        numRight = 0;
-        root = null;
-        super.setRight( null);
-        super.setLeft( null);   
-    }
-
-    // Constructor which sets key to the argument.
-    RBNode( T key ){
-        super(key); 
-        color = BLACK;
-        numLeft = 0;
-        numRight = 0;
-        root = null;
-        
-       
-    }
-    
-    RBNode( Node<T> aux) {        
-        super.setKey(aux.getKey());
-        super.setRight( aux.getRight());
-        super.setLeft( aux.getLeft());   
-        this.root = null;       
-     
-    }
 
 
     @Override
     public RBNode < T > getLeft() {
         RBNode< T > aux = null;
         if(super.getLeft().getKey() != null){
-            aux = new RBNode<> (super.getLeft());
-            aux.numLeft = this.getNumLeft();
+            return (RBNode<T>) (super.getLeft());
+           /* aux.numLeft = this.getNumLeft();
             aux.numRight = this.getNumRight();
             aux.root = this.getRoot();
-            aux.color  = this.getColor();       
+            aux.color  = this.getColor();     */  
         }
         return aux;
     }
@@ -88,14 +90,14 @@ package datastructures;
     @Override
     public RBNode < T > getRight() {
         RBNode< T > aux = null;
-        System.out.println(super.getRight().getKey());
         if(super.getRight().getKey() != null){
-            aux = new RBNode<> (super.getRight());
-            aux.numLeft = this.getNumLeft();
+            return (RBNode<T>) (super.getRight());
+           /* aux.numLeft = this.getNumLeft();
             aux.numRight = this.getNumRight();
             aux.root = this.getRoot();
-            aux.color  = this.getColor(); 
-        }  
+            aux.color  = this.getColor(); */
+        }
+        
         return aux;
     } 
 
